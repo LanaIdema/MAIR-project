b5870bd8ccfbfc1388b414c657ae112dea329338
NiekGrimbergen <niek01.grimbergen@gmail.com>
2026-09-23 17:19:48 +0200

    refactored step 2 w/ dec.trees

---
+++ output_step_2.md
@@ -0,0 +1,18 @@
+## mlp_classifier [Regular data]:
+- 0.8918461075903651 balanced_accuracy
+- 0.8822636343968212 f1-score
+## mlp_classifier [Stratisfied data]:
+- 0.7162859302606839 balanced_accuracy
+- 0.6857718326597998 f1-score
+## svm [Regular data]:
+- 0.8565755717244732 balanced_accuracy
+- 0.8701847824636733 f1-score
+## svm [Stratisfied data]:
+- 0.6486699339419263 balanced_accuracy
+- 0.6755502126915459 f1-score
+## decision_tree [Regular data]:
+- 0.9541981832462513 balanced_accuracy
+- 0.9405895865433316 f1-score
+## decision_tree [Stratisfied data]:
+- 0.6869907068540418 balanced_accuracy
+- 0.6546970166714939 f1-score
---
+++ step2.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+import pandas as pd
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, Normalizer
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import balanced_accuracy_score, f1_score
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+# Bag of words extractor, found on https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+from sklearn.feature_extraction.text import CountVectorizer
+
+def read_data(loc_training_data: str, loc_testing_data: str):
+    df_train = pd.read_csv(loc_training_data)
+    df_test = pd.read_csv(loc_testing_data)
+
+    # Two todo's for both: encode the labels to categorical features, based on trainingsdata
+    label_encoder = LabelEncoder()
+
+    X_train = df_train['text']
+    X_test = df_test['text']
+    y_train = label_encoder.fit_transform(df_train['label'])
+    y_test = label_encoder.transform(df_test['label'])
+
+    return {
+        "X_train": X_train,
+        "y_train": y_train, 
+        "X_test": X_test,
+        "y_test": y_test,
+        "label_encoder": label_encoder
+    }
+
+if __name__ == '__main__':
+    # Load the regular data
+    regular_data = read_data(
+        "./data/processed/original/train.csv", 
+        "./data/processed/original/test.csv"
+    )
+    # Load stratisfied data
+    stratisfied_data = read_data(
+        "./data/processed/grouped/train.csv", 
+        "./data/processed/grouped/test.csv"
+    )
+
+    datasets = [
+        ("Regular data", regular_data), 
+        ("Stratisfied data", stratisfied_data)
+    ]
+    model_types = [
+        ("mlp_classifier", MLPClassifier()),
+        ("svm", SVC()),
+        ("decision_tree", DecisionTreeClassifier())
+    ]
+
+    FILENAME = "output_step_2.md"
+    with open(FILENAME, "w", encoding="utf-8") as file:
+        for model_type in model_types:
+            for dataset_name, dataset in datasets:
+                pipe = Pipeline([
+                    ('bag-of-words-vectorizer', CountVectorizer()), # Count Vectorizer ignores unknown words, creates a bag of words representation
+                    ('normalizer', Normalizer('l2')), # Normalize the bag-of-words vectors to optimize distance calculations
+                    model_type # Chain the model in the pipe
+                ])
+
+                # Fit the model to the data
+                model = pipe.fit(dataset["X_train"], dataset["y_train"])
+                # Predict the test set
+                pred_y = model.predict(dataset["X_test"])
+                # Report
+                file.write(f"## {model_type[0]} [{dataset_name}]:\n")
+                file.write(f"- {balanced_accuracy_score(dataset["y_test"], pred_y)} balanced_accuracy\n")
+                file.write(f"- {f1_score(dataset["y_test"], pred_y, average='macro')} f1-score\n")
+    print("Done...")
+
+            
