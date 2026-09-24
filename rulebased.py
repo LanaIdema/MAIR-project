9516b7b238f939a7a34332f6aaccbfc8ce1fbd41
Lana <l.idema@uu.nl>
2026-09-24 11:03:59 +0200

    Added metrics to rule-based baseline

--- rulebased.py
+++ rulebased.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import re
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score
 
 from rulebased_rules import RULES
 
@@ -30,7 +30,7 @@ def apply_rulebased(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def calculate_accuracy_rulebased(df: pd.DataFrame) -> float:
+def calculate_metrics_rulebased(df: pd.DataFrame) -> float:
     # We need to replace the null values with the string " null"
     # to avoid errors when calculating the accuracy
     df["label"] = df["label"].fillna("null")
@@ -38,22 +38,30 @@ def calculate_accuracy_rulebased(df: pd.DataFrame) -> float:
 
     predicted_labels = df["prediction"]
 
-    return accuracy_score(true_labels, predicted_labels)
+    return accuracy_score(true_labels, predicted_labels), balanced_accuracy_score(true_labels, predicted_labels), f1_score(true_labels, predicted_labels, average="macro", zero_division=0)
 
 
 # calculate accuracy on the train set
 df_train = pd.read_csv("data/processed/grouped/train.csv")
 df_train_predictions = apply_rulebased(df_train)
 
-accuracy = calculate_accuracy_rulebased(df_train_predictions)
+accuracy, balanced_accuracy, macro_F1 = calculate_metrics_rulebased(df_train_predictions)
 print(f"Rule-based accuracy train: {accuracy}")
+print(f"Rule-based accuracy train: {balanced_accuracy}")
+print(f"Rule-based accuracy train: {macro_F1}")
 # Rule-based accuracy train: 0.9189981732525718
+# Rule-based accuracy train: 0.846409270894487
+# Rule-based accuracy train: 0.806821034238376
 
 
 # calculate accuracy on the test set
 df_test = pd.read_csv("data/processed/grouped/test.csv")
 df_test_predictions = apply_rulebased(df_test)
 
-accuracy = calculate_accuracy_rulebased(df_test_predictions)
+accuracy, balanced_accuracy, macro_F1 = calculate_metrics_rulebased(df_test_predictions)
 print(f"Rule-based accuracy test: {accuracy}")
+print(f"Rule-based accuracy test: {balanced_accuracy}")
+print(f"Rule-based accuracy test: {macro_F1}")
 # Rule-based accuracy test: 0.8967459324155194
+# Rule-based accuracy test: 0.7747955222295051
+# Rule-based accuracy test: 0.7265835369351522
